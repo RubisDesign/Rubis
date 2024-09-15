45b7a560f5211bb2009dbfdf7eda3906d60993ae
Remi13Git <remi.hnwi@gmail.com>
2024-09-15 17:09:16 +0200

    update index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+import logo from './rubis-bg.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Rubis Design
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My first react App
-        </p>
       </header>
     </div>
   );
